a4db2dd15fb2c62abf1888bb9668cac3a25504cc
huangpengju <15713716933@163.com>
2023-07-09 23:06:33 +0800

    chichat代码

--- Chapter_2_Go_ChitChat/chitchat/main.go
+++ Chapter_2_Go_ChitChat/chitchat/main.go
@@ -1,45 +1,48 @@
 package main
 
 import (
-	"chitchat/data"
-	"html/template"
 	"net/http"
+	"time"
 )
 
 func main() {
+	p("ChitChat", version(), "started at", config.Address)
 
-	// net/http 标准库提供了一个默认的多路复用器，通过NewServeMux()函数创建
-	// 创建一个多路复用器（多路转接器）
+	// handle static assets
 	mux := http.NewServeMux()
-	// fmt.Println("mux=", mux) // mux= &{{{0 0} 0 0 0 0} map[] [] false}
-	// fmt.Printf("%T", mux)    // *http.ServeMux
+	files := http.FileServer(http.Dir(config.Static))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
 
-	// 使用FileServer函数创建一个能够为指定目录中的静态文件服务的处理器
-	files := http.FileServer(http.Dir("/public"))
-	// 并将这个处理器传递给多路复用器的Handel函数
-	mux.Handle("/static/", http.StripPrefix("/static/", files)) // 使用StripPrefix函数移除请求URL中的指定前缀
-	// 服务器接收到一个以/static/开头的URL请求时，以上两行代码会移除URL中的/static/
-	// 然后在public目录中查找被请求的文件。
+	//
+	// all route patterns matched here
+	// route handler functions defined in other files
+	//
 
-	// 为了将发送至根URL的请求重定向到处理器，使用HandleFunc函数
-	// HandleFunc接受一个URL和一个处理器的名字作为参数
-	mux.HandleFunc("/", index) // HandleFunc函数把请求重定向到处理器函数
-	// server := &http.Server{
-	// 	Addr:    "0.0.0.0:8080",
-	// 	Handler: mux,
-	// }
-	// server.ListenAndServe()
-}
+	// index
+	mux.HandleFunc("/", index)
+	// error
+	mux.HandleFunc("/err", err)
 
-// index是处理器函数
-// 处理器函数实际上就是一个接受ResponseWriter和Request指针作为参数的Go函数
-func index(w http.ResponseWriter, r *http.Request) {
-	files := []string{"templates/layout.html",
-		"templates/navbar.html",
-		"templates/index.html"}
-	templates := template.Must(template.ParseFiles(files...))
-	threads, err := data.Threads()
-	if err == nil {
-		templates.ExecuteTemplate(w, "layout", threads)
+	// defined in route_auth.go
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/logout", logout)
+	mux.HandleFunc("/signup", signup)
+	mux.HandleFunc("/signup_account", signupAccount)
+	mux.HandleFunc("/authenticate", authenticate)
+
+	// defined in route_thread.go
+	mux.HandleFunc("/thread/new", newThread)
+	mux.HandleFunc("/thread/create", createThread)
+	mux.HandleFunc("/thread/post", postThread)
+	mux.HandleFunc("/thread/read", readThread)
+
+	// starting up the server
+	server := &http.Server{
+		Addr:           config.Address,
+		Handler:        mux,
+		ReadTimeout:    time.Duration(config.ReadTimeout * int64(time.Second)),
+		WriteTimeout:   time.Duration(config.WriteTimeout * int64(time.Second)),
+		MaxHeaderBytes: 1 << 20,
 	}
+	server.ListenAndServe()
 }
---
+++ Chapter_2_Go_ChitChat/chitchat/print_main.go
@@ -0,0 +1,8 @@
+package main
+
+// mainPrint 用来打印输出main()的一些内容，做好注释说明，帮助理解代码
+// func mainPrint() {
+// 	fmt.Println("____________这里是mainPrint()_____________")
+// 	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("/go.mod"))))
+
+// }
